d5d7f61e4f5314d51f4d58c60a22d82ee8d0d8ed
Chris Redford <credford@gmail.com>
2019-01-21 06:52:15 -0800

    initial master tests

--- maptemplate/mastertemplate/map_string_int_test.go
+++ maptemplate/mastertemplate/map_string_int_test.go
@@ -1,1 +1,31 @@
 package mastertemplate
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ContainsKeyStringInt_WhenContains_ReturnsTrue(t *testing.T) {
+	m := map[string]int{"a": 1, "b": 2, "c": 3}
+
+	result := ContainsKeyStringInt(m, "b")
+
+	assert.True(t, result)
+}
+
+func Test_ContainsKeyStringInt_WhenDoesNotContainStringValue_ReturnsFalse(t *testing.T) {
+	m := map[string]int{"a": 1, "b": 2, "c": 3}
+
+	result := ContainsKeyStringInt(m, "sandwich")
+
+	assert.False(t, result)
+}
+
+func Test_ContainsKeyStringInt_WhenMapEmpty_ReturnsFalse(t *testing.T) {
+	m := map[string]int{}
+
+	result := ContainsKeyStringInt(m, "a")
+
+	assert.False(t, result)
+}
