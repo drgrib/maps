5b04b2eeae4d777e40e5343b345478944eef820e
Chris Redford <credford@gmail.com>
2019-01-21 13:44:44 -0800

    getMasterTemplateString

--- maptemplate/mastertemplate/generate/main.go
+++ maptemplate/mastertemplate/generate/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
+	"strings"
 )
 
 const (
@@ -11,14 +11,37 @@ const (
 )
 
 func main() {
-	s := getMasterFileString()
-	fmt.Println(s)
+	masterTemplateString := getMasterTemplateString()
+	fmt.Println(masterTemplateString)
+}
+
+func getMasterTemplateString() string {
+	masterFileString := getMasterFileString()
+	replacements := map[string]string{
+		"mastertemplate": "{{.Package}}",
+		"String":         "{{.KeySuffix}}",
+		"Int":            "{{.ValueSuffix}}",
+		"string":         "{{.KeyType}}",
+		"int":            "{{.ValueType}}",
+	}
+	replacedFileString := applyReplacements(masterFileString, replacements)
+	return replacedFileString
 }
 
 func getMasterFileString() string {
 	b, err := ioutil.ReadFile(masterFilePath)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	return string(b)
+}
+
+func applyReplacements(original string, replacements map[string]string) string {
+	replaced := original
+
+	for old, new := range replacements {
+		replaced = strings.Replace(replaced, old, new, -1)
+	}
+
+	return replaced
 }
