f36e8dbc1ed62ea1f77eea8b2b135c91773359f1
Chris Redford <credford@gmail.com>
2019-01-21 07:04:40 -0800

    ContainsValueStringInt tests

--- maptemplate/mastertemplate/map_string_int_test.go
+++ maptemplate/mastertemplate/map_string_int_test.go
@@ -29,3 +29,27 @@ func Test_ContainsKeyStringInt_WhenMapEmpty_ReturnsFalse(t *testing.T) {
 
 	assert.False(t, result)
 }
+
+func Test_ContainsValueStringInt_WhenContains_ReturnsTrue(t *testing.T) {
+	m := map[string]int{"a": 1, "b": 2, "c": 3}
+
+	result := ContainsValueStringInt(m, 3)
+
+	assert.True(t, result)
+}
+
+func Test_ContainsValueStringInt_WhenDoesNotContainStringValue_ReturnsFalse(t *testing.T) {
+	m := map[string]int{"a": 1, "b": 2, "c": 3}
+
+	result := ContainsValueStringInt(m, 42)
+
+	assert.False(t, result)
+}
+
+func Test_ContainsValueStringInt_WhenMapEmpty_ReturnsFalse(t *testing.T) {
+	m := map[string]int{}
+
+	result := ContainsValueStringInt(m, 1)
+
+	assert.False(t, result)
+}
